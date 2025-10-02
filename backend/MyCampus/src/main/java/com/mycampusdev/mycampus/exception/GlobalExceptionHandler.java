e3b6b44455904fa8b8e5aa5715d2d38dae84dfd6
Lizad39 <3165944192@qq.com>
2025-10-02 23:43:39 +0800

    增加注释，完善用户模块部分代码

---
+++ backend/MyCampus/src/main/java/com/mycampusdev/mycampus/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.mycampusdev.mycampus.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.mycampusdev.mycampus.pojo.ResponseMessage;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestControllerAdvice  // 统一异常处理
+public class GlobalExceptionHandler {
+
+    Logger log = org.slf4j.LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler({Exception.class})  // 指定要处理的异常类型(所有的)
+    public ResponseMessage exceptionHandler(Exception e,HttpServletRequest request,HttpServletResponse response){
+        log.error("统一异常：",e);
+
+        return new ResponseMessage(500, e.getMessage(), "Internal Server Error.");
+    }
+}
--- backend/MyCampus/src/main/java/com/mycampusdev/mycampus/service/IUserService.java
+++ backend/MyCampus/src/main/java/com/mycampusdev/mycampus/service/IUserService.java
@@ -1,7 +1,84 @@
 package com.mycampusdev.mycampus.service;
 
 import com.mycampusdev.mycampus.pojo.User;
+import com.mycampusdev.mycampus.pojo.User.Address;
+import com.mycampusdev.mycampus.pojo.User.RunnerStatus;
 
+import java.util.Map;
+
+/**
+ * 用户服务的接口，定义了用户模块所需的所有业务方法。
+ */
 public interface IUserService {
-    void add(User user);
+
+    /**
+     * 用户注册
+     * @param user 包含注册信息的用户对象
+     * @return 创建成功并持久化的用户对象
+     */
+    User register(User user);
+
+    /**
+     * 用户登录
+     * @param username 用户名
+     * @param password 密码
+     * @return 包含认证token和用户信息的Map
+     */
+    Map<String, Object> login(String username, String password);
+
+    /**
+     * 根据ID查找用户
+     * @param id 用户的唯一ID
+     * @return 查找到的用户对象
+     */
+    User findById(String id);
+
+    /**
+     * 更新用户信息
+     * @param id 要更新的用户的ID
+     * @param userUpdates 包含待更新字段的Map
+     * @return 更新后的用户对象
+     */
+    User updateUser(String id, Map<String, Object> userUpdates);
+
+    /**
+     * 为用户添加新的收货地址
+     * @param userId 用户的ID
+     * @param address 新的地址信息
+     * @return 更新后的用户对象
+     */
+    User addAddress(String userId, Address address);
+
+    /**
+     * 更新用户的某个收货地址
+     * @param userId 用户的ID
+     * @param addressId 要更新的地址的ID
+     * @param addressUpdates 包含地址更新信息的对象
+     * @return 更新后的用户对象
+     */
+    User updateAddress(String userId, String addressId, Address addressUpdates);
+
+    /**
+     * 删除用户的某个收货地址
+     * @param userId 用户的ID
+     * @param addressId 要删除的地址的ID
+     */
+    void deleteAddress(String userId, String addressId);
+
+    /**
+     * 用户申请成为跑腿员
+     * @param userId 申请用户的ID
+     * @param idCardNumber 身份证号
+     * @param healthCertificateUrl 健康证URL
+     * @return 更新后的用户对象
+     */
+    User becomeRunner(String userId, String idCardNumber, String healthCertificateUrl);
+
+    /**
+     * 跑腿员更新自己的在线状态
+     * @param userId 跑腿员的用户ID
+     * @param status 新的在线状态
+     * @return 更新后的用户对象
+     */
+    User updateRunnerStatus(String userId, RunnerStatus status);
 }
